d3697cdc9cc605b6d037d9eeef1e24a8c69ca9ce
Vadim <_s_e_t__@ukr.net>
2025-02-24 17:11:55 +0200

    add some code

--- screens/SignupScreen.jsx
+++ screens/SignupScreen.jsx
@@ -1,26 +1,30 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 import { createUser } from "../util/auth";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticatig] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   const handlerSighUp = async ({ email, password }) => {
     setIsAuthenticatig(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed!",
         "Could not create user, please check your input and try again later."
       );
+      setIsAuthenticatig(false);
     }
 
-    setIsAuthenticatig(false);
   };
 
   if (isAuthenticating) {
--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -1,10 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 function WelcomeScreen() {
+  const [fetchMessage, setFetchMessage] = useState('');
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      try {
+        const response = await axios.get(
+          "https://react-native-authenticat-6e673-default-rtdb.firebaseio.com/message.json"
+        );
+        setFetchMessage(response.data);
+      } catch (error) {
+        throw new Error();
+      }
+    };
+    fetchMessage();
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchMessage}</Text>
     </View>
   );
 }
@@ -24,4 +43,3 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
 });
-
